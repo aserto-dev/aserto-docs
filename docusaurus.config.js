6382168e2e5b52a68569d24a6bb2347f9d478e63
Omri Gazitt <ogazitt@gmail.com>
2022-06-22 18:28:27 -0700

    turn off trailing slash in docusaurus.config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
     favicon: "img/favicon.ico",
     organizationName: "aserto-dev",
     projectName: "aserto-docs",
+    trailingSlash: false,
 
     plugins: [
       ['docusaurus2-dotenv',
