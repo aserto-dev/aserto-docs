b276a86866fcea93113b82abaa222bd8d89cec30
Omri Gazitt <ogazitt@gmail.com>
2023-09-21 22:41:22 -0700

    using react-svg instead of img to override fill in css

--- src/components/QuickstartPage/Quickstart/index.js
+++ src/components/QuickstartPage/Quickstart/index.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import Link from '@docusaurus/Link'
 import styles from './Quickstart.module.css'
+import { ReactSVG } from 'react-svg'
 
 const Sdk = ({ title, link, icon, description, docs }) => {
   return (
     <div className={styles.card}>
-      <a href={link}>
-        <img className={styles.cardImage} src={`/quickstarts/${icon}`} alt={title} />
-      </a>
-      <p className={styles.cardTitle}>{title}</p>
-      <br />
-      <p className={styles.cardDescription}>{description}</p>
       <Link href={docs}>
-        <button className={styles.launchButton}>View Quickstart</button>
+        {
+          icon === 'react.svg' ? 
+          <img className={styles.cardImage} src={`/quickstarts/${icon}`} alt={title} /> :
+          <div className={styles.cardImage}>
+            <ReactSVG src={`/quickstarts/${icon}`} alt={title} />
+          </div>
+        }
+        <p className={styles.cardTitle}>{title}</p>
+        <br />
+        <p className={styles.cardDescription}>{description}</p>
+        <Link href={docs}>
+          <button className={styles.launchButton}>View Quickstart</button>
+        </Link>
       </Link>
-    </div>    
+    </div>
   )
 }
 
