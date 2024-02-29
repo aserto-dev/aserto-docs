b7fac4257d954b383bb06438ababaae87a9a8a92
Omri Gazitt <ogazitt@gmail.com>
2024-02-28 22:14:53 -0800

    refined quickstart overview page (#192)
    * refined quickstart overview page
    
    * updated styles

--- src/components/QuickstartPage/index.js
+++ src/components/QuickstartPage/index.js
@@ -1,27 +1,66 @@
 import React from 'react'
-import sdks from '../../quickstarts'
+import quickstarts from '../../quickstarts'
 import Quickstart from './Quickstart'
-import styles from './QuickstartPage.module.css';
+import styles from './QuickstartPage.module.css'
 
 const QuickstartPage = () => {
-    return (
-        <div>
-            <p className={styles.pageTitle}>Get started in 5 minutes with one of these quickstarts.</p>
+  return (
+    <div>
+      <p className={styles.pageTitle}>
+        Get started in 5 minutes with one of these quickstarts.
+      </p>
 
-            <div className={styles.wrapper}>
-                {sdks.map((s) => 
-                    <Quickstart
-                        key={s.title}
-                        title={s.title}
-                        icon={s.icon}
-                        link={s.docs}
-                        description={s.description}
-                        docs={s.docs}
-                    />
-                )}
-            </div>
-        </div>    
-    )
+      <hr />
+      <h2 className={styles.pageSubtitle}>End-to-end samples</h2>
+      <div className={styles.wrapper}>
+        {quickstarts
+          .filter((q) => q.category === 'end-to-end-app')
+          .map((s) => (
+            <Quickstart
+              key={s.title}
+              title={s.title}
+              icon={s.icon}
+              link={s.docs}
+              description={s.description}
+              docs={s.docs}
+            />
+          ))}
+      </div>
+
+      <hr />
+      <h2 className={styles.pageSubtitle}>Authorization templates</h2>
+      <div className={styles.wrapper}>
+        {quickstarts
+          .filter((q) => q.category === 'template')
+          .map((s) => (
+          <Quickstart
+            key={s.title}
+            title={s.title}
+            icon={s.icon}
+            link={s.docs}
+            description={s.description}
+            docs={s.docs}
+          />
+        ))}
+      </div>
+
+      <hr />
+      <h2 className={styles.pageSubtitle}>Tutorials</h2>
+      <div className={styles.wrapper}>
+        {quickstarts
+          .filter((q) => q.category === 'tutorial')
+          .map((s) => (
+          <Quickstart
+            key={s.title}
+            title={s.title}
+            icon={s.icon}
+            link={s.docs}
+            description={s.description}
+            docs={s.docs}
+          />
+        ))}
+      </div>    </div>
+  )
 }
 
 export default QuickstartPage
