d74f3d0605e97e8393f52f3ab9a51ce10a903e33
Omri Gazitt <ogazitt@gmail.com>
2024-02-28 21:59:37 -0800

    refined quickstart overview page

--- src/quickstarts.js
+++ src/quickstarts.js
@@ -4,18 +4,42 @@ export default [
     icon: 'todo-1.svg',
     description: 'A familiar Todo sample, with a React front-end and a back-end written in six different languages, demonstrating how to integrate Aserto into your own app. Also includes a tutorial on the Aserto Policy language.',
     docs: '/docs/quickstarts/todo/overview',
+    category: 'end-to-end-app',
   },
   {
     title: 'Peoplefinder',
     icon: 'people-finder.svg',
     description: 'A corporate directory sample that contains a React front-end and a Node.js backend in a monorepo. It includes 3 different policy styles - RBAC, ABAC, and ReBAC. It also shows how to build a front-end that conditionally renders UI based on the user\'s permissions.',
     docs: '/docs/getting-started/peoplefinder/peoplefinder-overview',
+    category: 'end-to-end-app',
   },
   {
     title: 'Simple RBAC',
     icon: 'simple-rbac.svg',
-    description: 'A simple RBAC scenario, with a back-end written in six different languages. This is our simplest relationship-based access control (ReBAC) scenario.',
+    description: 'A model that supports resource owners, writers, and readers, with a simple back-end written in six different languages. This is our simplest relationship-based access control (ReBAC) scenario.',
     docs: '/docs/quickstarts/simple-rbac/overview',
+    category: 'template',
+  },
+  {
+    title: 'Google Drive',
+    icon: 'gdrive.svg',
+    description: 'A model that supports nested folders and documents, with a simple back-end written in six different languages. This demonstrates a hierarchical nested ReBAC scenario.',
+    docs: '/docs/quickstarts/gdrive/overview',
+    category: 'template',
+  },
+  {
+    title: 'Slack',
+    icon: 'slack.svg',
+    description: 'A model that mimics Slack\'s workspaces and channels, with a simple back-end written in six different languages. You can evolve it to fit your needs.',
+    docs: '/docs/quickstarts/slack/overview',
+    category: 'template',
+  },
+  {
+    title: 'GitHub',
+    icon: 'github.svg',
+    description: 'A model that mimics GitHub\'s organizations and repos, with a full set of inherited permissions, and a simple back-end written in six different languages. This is a good real-world example of a full authorization model.',
+    docs: '/docs/quickstarts/github/overview',
+    category: 'template',
   },
 /*
   {
@@ -23,18 +47,14 @@ export default [
     icon: 'multi-tenant-rbac.svg',
     description: 'Multi-tenant RBAC.',
     docs: '/docs/quickstarts/react/overview',
+    category: 'template',
   },
   {
     title: 'Tenants and Teams',
     icon: 'tenant-and-team.svg',
     description: 'Tenants and Teams.',
     docs: '/docs/quickstarts/react/overview',
-  },
-  {
-    title: 'Google Docs',
-    icon: 'google-doc.svg',
-    description: 'Google Docs.',
-    docs: '/docs/quickstarts/react/overview',
+    category: 'template',
   },
   */
   {
@@ -42,5 +62,6 @@ export default [
     icon: 'react.svg',
     description: 'A step-by-step tutorial on how to build a React and Node.js application from scratch using Aserto for authorization.',
     docs: '/docs/quickstarts/react/overview',
+    category: 'tutorial',
   },
 ]
