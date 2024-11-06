19a53f81f65af60a2b90cd2003946e4186658322
Gheorghe Popescu <gheorghe.popescu@aserto.com>
2024-11-06 04:14:38 +0200

    docusaurs 3 upgrade (#205)
    * docusaurs 3 upgrade
    
    * update CI
    
    * fix build
    
    * fixed markdown warning
    
    ---------
    
    Co-authored-by: Omri Gazitt <ogazitt@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,6 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+import {themes as prismThemes} from 'prism-react-renderer';
+const lightCodeTheme = prismThemes.github;
+const darkCodeTheme = prismThemes.dracula;
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
